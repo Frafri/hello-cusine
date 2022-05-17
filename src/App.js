618124a2433636ffd7bced9a7dc3ef6857de461c
Tatsiana <t.migai.91@gmail.com>
2022-05-17 11:45:49 +0100

    header component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1><img data-visualcompletion="media-vc-image" alt="No photo description available." class="ji94ytn4 d2edcug0 r9f5tntg r0294ipz" referrerpolicy="origin-when-cross-origin" src="https://scontent-lhr8-2.xx.fbcdn.net/v/t1.18169-9/18519720_498840877114607_4619815614151521331_n.png?_nc_cat=104&amp;_nc_map=control&amp;ccb=1-5&amp;_nc_sid=09cbfe&amp;_nc_ohc=JAQfvp1Eb-UAX-2dumg&amp;_nc_ht=scontent-lhr8-2.xx&amp;oh=00_AT9Cyv-qlyA0ZDgJEFkgP-6hsnTtreMDb_rzSyWHurlvJw&amp;oe=628A15BA"width="50" height="50"/>DolceMenteBaking</h1>
-      <header className="App-header">
-        <nav className="App-table">
-            <div> <a href="https://dolcementebaking.wixsite.com/dolcemente"> Home</a> </div>
-            <div> <a href="https://dolcementebaking.wixsite.com/dolcemente/food-from-around-the-world"> Recipes</a> </div>
-            <div> <a href="https://www.instagram.com/dolcementebaking/"> Photoes</a> </div>
-            <div> <a href="https://dolcementebaking.wixsite.com/dolcemente/about-me"> About me</a> </div>
-        </nav>
-      </header>
+        <Header />
         <a
           className="App-link"
           href="https://dolcementebaking.wixsite.com/dolcemente/food-from-around-the-world"
