b8d3ef73a9c85815bb72f1232ac48fcfdc560811
Francesca Macchiarini <fmacchiarini@fb.com>
2022-07-06 15:01:13 +0100

    withhome

--- src/App.js
+++ src/App.js
@@ -1,45 +1,17 @@
 import './App.css';
 import Header from './Header';
-import './Footer.css';
 import Footer from './Footer.js';
+import {Routes, Route} from 'react-router-dom';
+import Home from './Home.js';
+
 
 function App() {
   return (
     <div className="App">
         <Header />
-        <a
-          className="App-link"
-          href="https://dolcementebaking.wixsite.com/dolcemente/food-from-around-the-world"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Recipes
-        </a>
-        <div className="Roses-Image"> <img src="roses.png" alt="roses image" width="300" height="300"/>
-        <div className="Apple-Roses"><h2> Apple Roses </h2>
-        <p> These are super easy, but they are super nice to taste and to look! Why not trying? </p>
-        <u> Ingredients</u>
-        <li> 2 apples </li>
-        <li>1 pack of puff pastry</li>
-        <li>2 tbsp sugar</li>
-        <li>knob of butter</li>
-        <li>some water</li>
-        <li>2 tsp cinnamon</li>
-        <li>1 lemon juice</li>
-        </div></div>
-
-
-        <div className="Creme"> <img src="creme.png" alt="creme image" width="300" height="300"/>
-        <div className="Cremebrule"><h2> Crème Brûlée </h2>
-        <p> I have never made a crème brûlée before, but I have always loved them! <br/> So in these days of "stay at home" due to the virus, I decided I should give it a try! <br/> And, omg, it was AMAZING! It was creamy enough, the caramel on top was crunchy <br/> enough, it was just delicious!</p>
-        <u> Ingredients</u>
-        <li> 4 large eggs yolks </li>
-        <li>25g caster sugar</li>
-        <li>1 tbs Vanilla extract</li>
-        <li>600 ml double cream</li>
-        <li>50 g sugar for the caramel</li>
-        </div></div>
-
+          <Routes>
+          <Route path="/" element={Home}/>
+          </Routes>
         <Footer  />
     </div>
   );
