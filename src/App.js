46c6d076ddfccc45dd4032a4200c9394eba6de3a
Francesca Macchiarini <fmacchiarini@fb.com>
2022-07-06 14:33:41 +0100

    withfooter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import './Footer.css';
+import Footer from './Footer.js';
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Recipes
-        </a> <div className="Roses-Image"> <img src="roses.png" alt="roses image" width="300" height="300"/>
+        </a>
+        <div className="Roses-Image"> <img src="roses.png" alt="roses image" width="300" height="300"/>
         <div className="Apple-Roses"><h2> Apple Roses </h2>
         <p> These are super easy, but they are super nice to taste and to look! Why not trying? </p>
         <u> Ingredients</u>
@@ -25,16 +27,20 @@ function App() {
         <li>2 tsp cinnamon</li>
         <li>1 lemon juice</li>
         </div></div>
-      <div className="Facebook-Social-Logo">
-        <a href="https://www.facebook.com/dolcementebaking/">
-        <img class="lazy" src="https://www.logo.wine/a/logo/Facebook/Facebook-f_Logo-Blue-Logo.wine.svg" data-original="https://mpng.subpng.com/20180420/dye/kisspng-social-media-computer-icons-facebook-media-icon-5ad99bd52a6ec2.1244941515242106451738.jpg" title="Social Media Logo" alt="Social Media Logo - transparent png image" width="150" height="100"/>
-        </a>
-        <a href="https://www.instagram.com/dolcementebaking/">
-          <img alt="File:Instagram logo 2016.svg" src="https://www.logo.wine/a/logo/Instagram/Instagram-Glyph-Color-Logo.wine.svg" decoding="async" width="100" height="100" srcset="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/198px-Instagram_logo_2016.svg.png?20210403190622 1.5x, https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/264px-Instagram_logo_2016.svg.png?20210403190622 2x" data-file-width="100" data-file-height="10"/>
-        </a> </div>
-      <footer>
-        <p>Author: Francesca Macchiarini</p>
-      <p><a href="mailto:dolcementebaking@gmail.com">dolcementebaking@gmail.com</a></p></footer>
+
+
+        <div className="Creme"> <img src="creme.png" alt="creme image" width="300" height="300"/>
+        <div className="Cremebrule"><h2> Crème Brûlée </h2>
+        <p> I have never made a crème brûlée before, but I have always loved them! <br/> So in these days of "stay at home" due to the virus, I decided I should give it a try! <br/> And, omg, it was AMAZING! It was creamy enough, the caramel on top was crunchy <br/> enough, it was just delicious!</p>
+        <u> Ingredients</u>
+        <li> 4 large eggs yolks </li>
+        <li>25g caster sugar</li>
+        <li>1 tbs Vanilla extract</li>
+        <li>600 ml double cream</li>
+        <li>50 g sugar for the caramel</li>
+        </div></div>
+
+        <Footer  />
     </div>
   );
 }
